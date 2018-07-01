e201896b9f736321220bcb45a533abbb069d33fd
Maxime BORGES <maxime.borges@gmail.com>
2018-07-01 21:17:06 +0200

    Remove some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state',
+      ]
+    }],
+    'no-underscore-dangle': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint'
