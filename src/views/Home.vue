5c0f341e321c712f2e5bba12fab8ef10af7cee53
Maxime BORGES <maxime.borges@gmail.com>
2018-07-04 12:10:08 +0200

    Add LE POULPE on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="home">
+    <img src="img/poulpe_tmp.png"/>
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  bottom: 0;
+  position: absolute;
+  left: 0;
+}
+</style>
 
 <script>
 export default {
